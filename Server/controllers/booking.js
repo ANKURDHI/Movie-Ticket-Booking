1db1e5bd237752ff2215456474ef91db0ec7e1ce
Ankur <adkumar881@gmail.com>
2023-12-09 10:00:45 +0530

    getBooking

--- Server/controllers/booking.js
+++ Server/controllers/booking.js
@@ -9,11 +9,11 @@ const allBooking =  async(req,res)=>{
 }
 //get Booking
 const getBooking = async (req,res)=>{
-    const Booking_ID = req.body.Booking_ID;
+    const User_ID = req.body.User_ID;
   
     try{        
-        let q='SELECT * FROM Booking WHERE Booking_ID = ?;'
-        const [response] = await pool.query(q,[Booking_ID])
+        let q='SELECT Seats.*, Screen.*, Theatre.*, Movie.* FROM Seats JOIN Screen ON Seats.Screen_ID = Screen.Screen_ID JOIN Theatre ON Screen.Theatre_ID = Theatre.Theatre_ID JOIN show1 ON Screen.Screen_ID = show1.Screen_ID JOIN Movie ON show1.Movie_ID = Movie.Movie_ID WHERE Seats.User_ID = ?; '
+        const [response] = await pool.query(q,[User_ID])
         res.status(200).json(response)
     }
     catch(err){
